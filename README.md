c7927b7f2cd19971985d2763921ba739fe8600eb
Mateus Garcia <mateus.gigainfo@gmail.com>
2019-02-07 21:13:58 -0200

    feat(LAURA): Primeira versão funcional

---
+++ README.md
@@ -0,0 +1,28 @@
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)[![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg)](https://conventionalcommits.org)
+
+# Medicine Quality Control - mqc
+
+## Descrição
+Uma implementação da arquitetura IoT [L.A.U.R.A](https://laura-architecture.github.io/) para resolver o problema do [Medicine Quality Control](https://bit.ly/2TOp6lI) citado no convite para colaboração no projeto de Doutorado do Professor [Sérgio Teixeira](http://www.multicast.com.br/sergio/).
+
+## Requisitos mínimos do sistema
+
+<a href="https://nodejs.org/en/">Nodejs 8.11.3</a> ou superior  
+<a href="https://nodejs.org/en/">npm 6.5.0</a> ou superior  
+
+## Instalação das dependências
+Entre no diretorio do projeto e rode esse comando no terminal
+```bash
+npm install
+```
+
+## Configurando o app
+Abra o arquivo config.json e aponte os dados do seu cenário de teste local.
+
+## Executando o app
+após os passos anteriores, execute no terminal:
+
+```bash
+npm run start
+```
+
---
+++ index.js
@@ -0,0 +1,115 @@
+const W3CWebSocket = require( 'websocket' ).w3cwebsocket;
+const conf = require( './config.json' ); // arquivo de configuração
+const sendRequest = require( 'request-promise' ); // lib que faz requisições http
+const sleep = require( 'sleep' ); // funcionalidade para fazer o código aguardar
+
+console.clear();
+console.log( `\n[ INFO ] Tentando se conectar com ${conf.ServerAddress} ....\n` );
+var client = new W3CWebSocket( `ws://${conf.ServerAddress}:${conf.ServerPort}${conf.SocketEndPoint}` );
+
+client.onerror = function () {
+    console.log( `[ ERRO ] Falhou em tentar se conectar com ${ServerAddress}` );
+};
+
+client.onopen = function () {
+    console.log( '[ INFO ] Conexão estabelecida com sucesso !\n' );
+    console.log( `[ INFO ] Ouvindo a porta ${conf.ServerPort} no dispositivo ${conf.ServerAddress} ...\n\n` );
+};
+
+client.onclose = function () {
+    console.log( '[ INFO ] Cliente fechado.' );
+};
+
+client.onmessage = function ( e ) {
+    if ( typeof e.data === 'string' ) { //filtro pra não receber eventuais entulhos
+        var msg = JSON.parse( e.data );
+        if (
+            msg.type == "ACTIVATION"
+            && msg.situation.type == "mqc.AllETABiggerThanTTT"
+            && msg.situation.active == true
+        ) {
+            StartPipeline( msg );
+            console.log( "\n\n\nOK\n\n\n" );
+        }
+    }
+};
+
+
+/**
+ * Inicia o fluxo do Stage 3
+ */
+
+async function StartPipeline ( msg ) {
+    //força o programa a aguardar por 5 segundos antes de continuar
+    sleep.sleep( 5 );
+
+    //prepara os dados da requisição http que será feita
+    const options = {
+        uri: `http://${conf.ServerAddress}:${conf.ServerPort}${conf.SituationsEndPoint}/${msg.situation.id}`,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true // transforma automagicamente o resultado em json
+    };
+
+    var VerificationMsg; // receberá o resultado da requisição
+
+    await sendRequest( options )
+        .then( function ( situation ) {
+            VerificationMsg = situation;
+        } )
+        .catch( function ( err ) {
+            console.log( "[ ERRO ] A requisição falhou\n", err.message );
+        } );
+
+    if ( VerificationMsg.active == true ) {
+        console.log( `\n\n####################################################################` );
+        console.log( `####################### [!] WARNING [!] ############################` );
+        console.log( `--------------------------------------------------------------------` );
+        console.log( "TODOS OS OBSERVADORES ESTÃO LONGE DEMAIS PARA CHEGAR A TEMPO" );
+        console.log( `--------------------------------------------------------------------\n` );
+        console.log( `PRODUTO EM RISCO: ${VerificationMsg.participations.product.descriptor}` );
+        console.log( `TEMPERATURA MÁXIMA: ${VerificationMsg.participations.product.attributes.maxTemperature}` );
+        console.log( `####################################################################` );
+        console.log( `####################################################################\n\n` );
+    }
+
+    //força o programa a aguardar por mais 10 segundos antes de continuar
+    sleep.sleep( 10 );
+
+    // prepara os dados para a próxima requisição http que será feita
+    const options2 = {
+        uri: `http://${conf.ServerAddress}:${conf.ServerPort}${conf.SituationsEndPoint}?type=mqc.Attending&active=true`,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true // transforma automagicamente o resultado em json
+    };
+    var Situations; //receberá o array de situações
+
+    await sendRequest( options2 )
+        .then( function ( situations ) {
+            Situations = situations;
+        } )
+        .catch( function ( err ) {
+            console.log( "[ ERRO ] A requisição falhou\n", err.message );
+        } );
+
+    if ( Situations.length == 0 ) {
+        console.log( `\n\n####################################################################` );
+        console.log( `####################### [!] WARNING [!] ############################` );
+        console.log( `--------------------------------------------------------------------` );
+        console.log( "       NENHUM OBSERVADOR RESPONDEU AO CHAMADO ATÉ AGORA" );
+        console.log( `--------------------------------------------------------------------` );
+        console.log( `####################################################################` );
+        console.log( `####################################################################\n\n` );
+        StartPipeline( msg );
+    } else {
+        console.clear();
+        console.log( `--------------------------------------------------------------------` );
+        console.log( `--------------------------------------------------------------------` );
+        console.log( "[ INFO ] O problema ja foi atendido." );
+        console.log( `--------------------------------------------------------------------` );
+        console.log( `--------------------------------------------------------------------\n\n\n` );
+    }
+}
